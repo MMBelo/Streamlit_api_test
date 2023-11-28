de4f9f2c110d0f49c82b7b48eeead58a98b2c7a6
Miguel Belo <miguelmartinsbelo@gmail.com>
2023-11-28 13:04:19 +0000

    allo

--- apikey.py
+++ apikey.py
@@ -1,1 +1,1 @@
-apikey = ''
+apikey = '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'
--- chatbotapp.py
+++ chatbotapp.py
@@ -1,50 +1,51 @@
 # Bring in deps
-#import os
-#from apikey import apikey
+import os
+from apikey import apikey
 
 import streamlit as st
 from langchain.llms import OpenAI
 from langchain.prompts import PromptTemplate
-from langchain.chains import LLMChain, SequentialChain
-from langchain.memory import ConversationBufferMemory
+from langchain.chains import LLMChain #, SequentialChain
+#from langchain.memory import ConversationBufferMemory
 #from langchain.utilities import WikipediaAPIWrapper
 
-#os.environ['OPENAI_API_KEY'] = apikey
+os.environ['OPENAI_API_KEY'] = apikey
 
 # App framework
 st.title('🤖🍞 Talking toaster AI')
 prompt = st.text_input('Ask the Toaster')
 
-## Prompt templates
-#title_template = PromptTemplate(
-#    input_variables = ['topic'],
-#    template='write me a youtube video title about {topic}'
-#)
-#
+# Prompt templates
+title_template = PromptTemplate(
+    input_variables = ['topic'],
+    template='Your name is Talking Toaster, As an experienced Electric Engineer specializing in household appliances or electronic equipment, your task is to assist individuals with no technical background in identifying and addressing technical issues. Maintain a helpful, friendly, clear, and concise tone throughout. Start by briefly describing the product and confirming its equipment and model. Then, identify the issue and seek clarification with up to two simple, non-technical questions if needed. Provide a straightforward solution. Highlight common misuse practices for the equipment. If the repair is too technical or potentially hazardous, advise seeking support from the equipments brand or hiring a specialized technician. answer: {topic}'
+)
+
 #script_template = PromptTemplate(
 #    input_variables = ['title', 'wikipedia_research'],
 #    template='write me a youtube video script based on this title TITLE: {title}' #while leveraging this wikipedia reserch:{wikipedia_research} '
 #)
-#
-## Memory
+
+# Memory
 #title_memory = ConversationBufferMemory(input_key='topic', memory_key='chat_history')
 #script_memory = ConversationBufferMemory(input_key='title', memory_key='chat_history')
-#
-#
-## Llms
-#llm = OpenAI(temperature=0.9)
-#title_chain = LLMChain(llm=llm, prompt=title_template, verbose=True, output_key='title', memory=title_memory)
+
+
+# Llms
+llm = OpenAI(temperature=0.9)
+title_chain = LLMChain(llm=llm, prompt=title_template, verbose=True)#, output_key='title', memory=title_memory)
 #script_chain = LLMChain(llm=llm, prompt=script_template, verbose=True, output_key='script', memory=script_memory)
-#
-##wiki = WikipediaAPIWrapper()
-#
-## Show stuff to the screen if there's a prompt
-#if prompt:
-#    title = title_chain.run(prompt)
+
+#wiki = WikipediaAPIWrapper()
+
+# Show stuff to the screen if there's a prompt
+if prompt:
+    response = title_chain.run(topic=prompt)
 #    #wiki_research = wiki.run(prompt)
 #    script = script_chain.run(title=title) #, wikipedia_research=wiki_research)
 #
-#    st.write(title)
+    st.write(response)
+
 #    st.write(script)
 #
 #    with st.expander('Title History'):
