c6850b511f7e99a9cfbaf518601d3bfec75ff2bf
Miguel Belo <miguelmartinsbelo@gmail.com>
2023-11-29 23:38:44 +0000

    update requirements

--- apikey.py
+++ apikey.py
@@ -1,1 +1,1 @@
-apikey = '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'
+apikey = '"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"'
--- functions.py
+++ functions.py
@@ -1,3 +1,18 @@
-def object_detection(image):
-    object_name = model.detecte_image(image)
-    return object_name
+from chatbotapp import give_response
+
+def execute_1(uploaded_image):
+    image = uploaded_image
+    if image:
+        fridge = 'Samsung1000'
+        return fridge
+
+def send_product_name():
+    product = execute_1()
+    return product
+
+
+
+def retrieve_answer(prompt):
+    if prompt:
+        manual = give_response(prompt)
+        return manual
