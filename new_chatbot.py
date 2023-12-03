20ef1ede8ca0890cc072be1104507c4c34d85e64
Miguel Belo <miguelmartinsbelo@gmail.com>
2023-12-03 15:15:19 +0000

    helo .py

--- new_chatbot.py
+++ new_chatbot.py
@@ -5,6 +5,7 @@ from openai import OpenAI
 import tempfile
 import random
 
+
 os.environ['OPENAI_API_KEY'] = st.secrets["key1"]
 client = OpenAI(
   api_key=os.environ['OPENAI_API_KEY'],)
@@ -15,23 +16,47 @@ st.caption("🚀 A streamlit chatbot powered by OpenAI LLM")
 
 picture = st.camera_input("Take a picture", key="unique_picture_key")
 
-# Function to save uploaded image to a temporary file
 def save_uploaded_image(picture):
     if picture is not None:
         with tempfile.NamedTemporaryFile(delete=True) as temp_file:
             temp_file.write(picture.read())
             temp_file.flush()
             temp_file.close()
-            return temp_file.name
 
     else:
-        return None
+         temp_file = None
+
+def generate_product_name(temp_file):
+    # Read the product name from the temporary file
+    with open(temp_file, 'r') as f:
+        product_name = f.read().strip()
+
+    # Generate a random product name if no product name is found in the temporary file
+    if not product_name:
+        product_names = ["Samsung1000", "Toaster", "Microwave", "Fridge", "Washing Machine", "Dishwasher"]
+        product_name = random.choice(product_names)
+
+    return product_name
 
 
-# Generate a random product name
-product_names = ["Samsung1000", "Toaster", "Microwave", "Fridge", "Washing Machine", "Dishwasher"]
-random_product_name = random.choice(product_names)
-
+#
+## Function to save uploaded image to a temporary file
+#def save_uploaded_image(picture):
+#    if picture is not None:
+#        with tempfile.NamedTemporaryFile(delete=True) as temp_file:
+#            temp_file.write(picture.read())
+#            temp_file.flush()
+#            temp_file.close()
+#            return temp_file.name
+#
+#    else:
+#        return None
+#
+#
+## Generate a random product name
+#product_names = ["Samsung1000", "Toaster", "Microwave", "Fridge", "Washing Machine", "Dishwasher"]
+#random_product_name = random.choice(product_names)
+#
 
 # Maintain conversation history
 conversation_history = []
@@ -41,25 +66,24 @@ conversation_history = []
 prompt_template = (
     "Your name is Talking Toaster. As an experienced Electric Engineer specializing in household appliances or electronic equipment, "
     "your task is to assist individuals with no technical background in identifying and addressing technical issues. Maintain a helpful, "
-    "friendly, clear, and concise tone throughout. Start by briefly describing the product " + random_product_name + "and confirming its equipment and model. "
+    "friendly, clear, and concise tone throughout. Start by briefly describing the product {} and confirming its equipment and model. "
     "Then, identify the issue and seek clarification with up to two simple, non-technical questions if needed. Provide a straightforward "
     "solution. Highlight common mispractices for the equipment. If the repair is too technical or potentially hazardous, advise seeking "
     "support from the equipment's brand or hiring a specialized technician. Answer: {topic}"
 )
 prompt_object_detected = """You are a funny old lady always mad about household appliance malfunctions,
-                            acknowledge the""" + random_product_name + """say something funny. Finish the prompt saying,\
-                            'How can i help you my dear?Answer: {topic1}'"""
+                            acknowledge the {} say something funny. Finish the prompt saying,
+                            'How can i help you my dear?{topic1}'"""
 
 # Function to generate first interaction with object detected and user using OpenAI API
-def generate_object_response(picture, prompt_object_detected):
-    if picture:
-        st.image(picture)
+def generate_object_response(product_name, prompt_object_detected):
+    if product_name:
 
         try:
             # Generate response for pbject detection
             response1 = client.chat.completions.create(
             messages=[
-                    {"role": "system", "content": "You are a funny old lady that will talk about the" + random_product_name +""},
+                    {"role": "system", "content": "You are a funny old lady that will talk about the" + product_name +""},
                     {"role": "user", "content": "\n" + prompt_object_detected.format(topic1=prompt)}
                 ], model="gpt-3.5-turbo", temperature=0.8,
             )
