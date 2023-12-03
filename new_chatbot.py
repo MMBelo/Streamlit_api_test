7a5b8f2150829b603c21b82f7bf74229195322b3
Miguel Belo <miguelmartinsbelo@gmail.com>
2023-12-03 18:06:22 +0000

    helo .py

--- new_chatbot.py
+++ new_chatbot.py
@@ -39,7 +39,7 @@ if picture:
         st.error(f"Error saving image: {e}")
 
 if product_name:
-        st.success(f"Product Name: {product_name}")
+        product_name = st.success(f"Product Name: {product_name}")
 
 # Maintain conversation history
 conversation_history = []
@@ -60,7 +60,7 @@ prompt_object_detected = ("""You are a funny old lady always mad about household
 
 # Function to generate first interaction with object detected and user using OpenAI API
 def generate_object_response(product_name, prompt_object_detected):
-    if st.success(f"Product Name: {product_name}"):
+    if product_name:
         try:
             # Generate response for pbject detection
             response1 = client.chat.completions.create(
